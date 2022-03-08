5ef4a23754aed25e0c211aeb9d567cbe1edbcc30
chrs-m <mister.ch@gmail.com>
2022-03-08 16:30:26 +0100

    Initial commit

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿public class Customer
+{
+
+    public double moneyOwned;
+    public double moneyInserted;
+
+    public double GetRandomAmount(double min, double max)
+    {
+        double moneyAmount;
+        Random random = new Random();
+        moneyAmount = random.NextDouble() * (max - min) + min;
+        moneyAmount = Math.Round(moneyAmount, 0);
+        return moneyAmount;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Customer newCustomer = new Customer();
+newCustomer.moneyOwned = newCustomer.GetRandomAmount(20.0, 200.0);
+
+Console.WriteLine($"The customer has {newCustomer.moneyOwned}kr to spend.");
